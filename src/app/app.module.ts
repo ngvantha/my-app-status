eb9c953491489a1387954f20c5dc9fb23c2f454a
ngvtha <nguyentha.jp@gmail.com>
2022-09-24 20:34:58 +0900

    create layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './page/home/home.component';
 import { ProductComponent } from './page/product/product.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MenuSidenavComponent } from './layout/menu-sidenav/menu-sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProductComponent
+    ProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuSidenavComponent
   ],
   imports: [
     BrowserModule,
