cca0e8e6c3ad3e152e5a66ee7a846079fe77e773
ngvtha <nguyentha.jp@gmail.com>
2022-09-24 21:36:44 +0900

    material setting

--- src/app/layout/menu-sidenav/menu-sidenav.component.ts
+++ src/app/layout/menu-sidenav/menu-sidenav.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-sidenav.component.css']
 })
 export class MenuSidenavComponent implements OnInit {
-
+  showFiller = false;
   constructor() { }
 
   ngOnInit(): void {
